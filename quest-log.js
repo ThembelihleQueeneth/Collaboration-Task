b86954cf10f1017555880093393ed62497787719
ThembelihleQueeneth <malukathembelihle95@gmail.com>
2025-08-08 13:27:29 +0200

    Updated the quest log

--- quest-log.js
+++ quest-log.js
@@ -5,11 +5,16 @@ for(var i = 0; i<questLog.length; i++){
     console.log(questLog[i]);
 }
 
-
+//step 2
 console.log("The size of the log: "+questLog.length);
 
 //Step 3
 for(var j = 0; j<questLog.length; j++){
     console.log(questLog[0]);
     break;
+}
+//step 4
+questLog.push("Deliver the Royal Scroll");
+for(var i = 0; i<questLog.length; i++){
+    console.log("\n"+questLog[i]+"\n");
 }
