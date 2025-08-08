4fede3fc5027235d21a0b8901a0b16e0c66eb0ea
ThembelihleQueeneth <malukathembelihle95@gmail.com>
2025-08-08 11:54:53 +0200

    Declared and Assigned Variables

---
+++ icebreaker.js
@@ -0,0 +1,13 @@
+
+
+
+//Partner 2 → Responsible for Scenarios 5–8
+ var top_three_fav_food = ["Pizza", "Burger", "Ribs"];
+ var discount_code = "";
+ var user_profile_pic__URL = "";
+ const galatic_star_ID = "";
+
+ console.log("Top 3 favourite food:"+top_three_fav_food);
+ console.log("Discount code:"+discount_code);
+ console.log("User profile picture URL:"+user_profile_pic__URL);
+ console.log("Galactic Star ID:"+galatic_star_ID);
