0d31bb31b2eda878e805b4337055a0c3e4d58788
Ndima-karabo17 <mhangwanikarabo@gmail.com>
2025-08-08 13:16:33 +0200

    Update quest-log.js
    I added answer for step 1

---
+++ quest-log.js
@@ -0,0 +1,6 @@
+//Step 1
+const questLog = ["Collect 10 Firefly Wings" ,"Defeat the Goblin King" ,"Find the Lost Sword of Oakhaven"];
+
+for(var i = 0; i<questLog.length; i++){
+    console.log(questLog[i]);
+}
