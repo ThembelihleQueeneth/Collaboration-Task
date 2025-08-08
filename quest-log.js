74ef2d87b17bd29f406d43641536cbe8074ec5be
ThembelihleQueeneth <malukathembelihle95@gmail.com>
2025-08-08 13:32:47 +0200

    unshift

--- quest-log.js
+++ quest-log.js
@@ -5,7 +5,7 @@ for(var i = 0; i<questLog.length; i++){
     console.log(questLog[i]);
 }
 
-//step 2
+//Step 2
 console.log("The size of the log: "+questLog.length);
 
 //Step 3
@@ -13,8 +13,14 @@ for(var j = 0; j<questLog.length; j++){
     console.log(questLog[0]);
     break;
 }
-//step 4
+//Step 4
 questLog.push("Deliver the Royal Scroll");
+for(var i = 0; i<questLog.length; i++){
+    console.log("\n"+questLog[i]+"\n");
+}
+
+//Step 6
+questLog.unshift("Rescue the Blacksmith's Cat");
 for(var i = 0; i<questLog.length; i++){
     console.log("\n"+questLog[i]+"\n");
 }
